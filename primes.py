f14030c33ca67b4bca5723252cc05c82905acdc4
Rayan Popat <rayan.popat@kcl.ac.uk>
2023-10-11 15:24:38 +0100

    Add Primes v2 function that accounts for invalid inputs.

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    if number_of_primes < 1:
+        raise ValueError("Number of primes must be greater than 1")
+    else:
+        while len(list) < number_of_primes:
+            for i in range(2,num):
+                if num % i == 0:
+                    break
+            else:
+                    list.append(num)
+            num += 1
     return list
